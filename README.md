dee3272b82da797eccedcf9df07a55bb3090aba1
Gcccccccc <386759760@qq.com>
2021-03-17 16:24:48 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Ansible-Keylimeansible keylime
---
+++ fedora_33_setup_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd /root
+
+dnf install -y \
+  keylime \
+  wget \
+  gcc \
+  make \
+  openssl-devel \
+  vim \
+  iputils
+
+dnf install -y dbus \
+  dbus-daemon \
+  dbus-devel \
+  swtpm \
+  swtpm-tools
